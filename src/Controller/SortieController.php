459eb4caa1cb056d79c07ba7adc887209078277a
Gpoutier <poutiergatien1@gmail.com>
2023-06-02 21:38:50 +0200

    Pleins de changements

---
+++ src/Controller/SortieController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Sortie;
+use App\Form\SortieFormType;
+use App\Repository\EtatRepository;
+use App\Repository\SortieRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SortieController extends AbstractController
+{
+    #[Route('/sortie', name: 'create_sortie')]
+    public function index(Request $request, EntityManagerInterface $entityManager, EtatRepository $etatRepository): Response
+    {
+        $participant = $this ->getUser();
+        $sortie = new Sortie();
+        $formSortie = $this->createForm(SortieFormType::class, $sortie);
+
+        $formSortie->handleRequest($request);
+        if ($formSortie->isSubmitted() && $formSortie->isValid()) {
+            //On récupère les données du formulaire
+            $sortie = $formSortie->getData();
+            $sortie -> setCampus($participant -> getCampus());
+            $etat = $formSortie->get('enregistrer')->isClicked()
+                ? $etatRepository-> findOneBy(['libelle'=>'Créée'])
+                :$etatRepository-> findOneBy(['libelle'=>'Ouverte']);;
+            $sortie ->setEtat($etat);
+            $sortie -> setOrganisateur($participant);
+
+            $entityManager->persist($sortie);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'La sortie à bien été créée');
+        }
+
+        return $this->render('sortie/createSortie.html.twig', [
+            'formSortie' => $formSortie->createView(),
+            'participant' => $participant,
+        ]);
+    }
+
+    #[Route('/sortie/ConsulterSortie/{id}', name: 'consulter_sortie')]
+    public function consulterSortie(int $id, SortieRepository $sortieRepository)
+    {
+        $sortie = $sortieRepository -> findOneBy(['id' => $id]);
+
+
+        return $this->render('sortie/consulterSortie.html.twig', [
+            'sortie' => $sortie,
+        ]);
+    }
+
+    #[Route('/sortie/ModifierSortie/{id}', name: 'modif_sortie')]
+    public function ModifierSortie(int $id, SortieRepository $sortieRepository, Request $request, EntityManagerInterface $entityManager, EtatRepository $etatRepository)
+    {
+        $participant = $this ->getUser();
+        $sortie = $sortieRepository -> findOneBy(['id' => $id]);
+        $formSortie = $this->createForm(SortieFormType::class, $sortie);
+
+        $formSortie->handleRequest($request);
+        if ($formSortie->isSubmitted() && $formSortie->isValid()) {
+            //On récupère les données du formulaire
+            $sortie = $formSortie->getData();
+            if ($formSortie->get('enregistrer')->isClicked()) {
+                $etat = $etatRepository-> findOneBy(['libelle'=>'Créée']);
+            }
+            elseif ($formSortie->get('publier')->isClicked()) {
+                $etat = $etatRepository-> findOneBy(['libelle'=>'Ouverte']);
+            }
+            else {
+                $etat = $etatRepository-> findOneBy(['libelle'=>'Annulée']);
+            }
+            $sortie ->setEtat($etat);
+
+            $entityManager->persist($sortie);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'La sortie à bien été modifiée');
+        }
+
+
+        return $this->render('sortie/modifierSortie.html.twig', [
+             'formSortie' => $formSortie->createView(),
+             'sortie' =>$sortie,
+             'participant' => $participant,
+        ]);
+    }
+
+}
---
+++ src/Form/SortieFormType.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Campus;
+use App\Entity\Lieu;
+use App\Entity\Sortie;
+use App\Entity\Ville;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\ResetType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+
+class SortieFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom', TextType::class,[
+                'label'=>'Nom de la sortie : ',
+
+            ])
+            ->add('dateHeureDebut',DateTimeType::class,[
+                'label'=>'Date et Heure de la sortie :',
+                'html5'=>true,
+                'widget'=>'single_text',
+            ])
+
+            ->add('dateLimiteInscription', DateType::class,[
+                'label'=>'Date limite d\'inscription : ',
+                'html5'=>true,
+                'widget'=>'single_text',
+            ])
+            ->add('nbInscriptionsMax', IntegerType::class,[
+                'label'=>'Nombre de places : '
+            ])
+            ->add('duree', IntegerType::class,[
+                'label'=>'Durée (en minutes): '
+            ])
+            ->add('infosSortie', TextareaType::class,[
+                'label'=>'Description et infos : '
+            ])
+            ->add('ville', EntityType::class,[
+                'mapped'=> false,
+                'label'=>'Ville : ',
+                'class'=> Ville::class,
+                'choice_label'=>'nom'
+            ])
+            ->add('lieu',EntityType::class,[
+                'label' => 'lieu : ',
+                'class' => Lieu::class,
+                'choice_label' => 'nom'
+            ])
+            ->add('enregistrer',SubmitType::class,[
+                'label'=>'Enregistrer'
+            ])
+            ->add('publier',SubmitType::class,[
+                'label'=>'Publier'
+            ])
+
+            ->add('annuler',ResetType::class,[
+                'label'=>'Annuler',
+                'attr' => [
+                    'class'=> 'btn btn-secondary ml-auto',
+                    'onclick' => sprintf("window.location.href = '%s';", $this->router->generate('app_accueil'))
+                ]
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Sortie::class,
+        ]);
+    }
+
+    public function __construct(UrlGeneratorInterface $router)
+    {
+        $this->router = $router;
+    }
+}
